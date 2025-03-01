12f738eeb32dfab71fb4eaf0dcb49bf9b0fc4b77
JasdewStarfield <mysteriousender@163.com>
2025-03-01 20:27:13 +0800

    Debug

--- src/handlers/memory.py
+++ src/handlers/memory.py
@@ -10,7 +10,7 @@ bot_name = "布兰妮"
 memory_system_prompt = "以下是发生在用户和暴风雪航天飞机拟人AI“布兰妮”（一个有着14岁外表的少女，实际上是搭载在少女仿生体上的苏联AI）的微信聊天记录。请将以下对话记录总结为最重要的几条长期记忆，总结内容应包含地点，事件，人物（如果对话记录中有的话），用中文表述，无需重复人物设定，尽可能简要但包含所有重要细节："
 persona_system_prompt = "以下我将给出一段基于发生在用户和暴风雪航天飞机拟人AI“布兰妮”（一个有着14岁外表的少女，实际上是搭载在少女仿生体上的苏联AI）之间的微信聊天的总结，还将给出一段先前的布兰妮对用户形象的印象。你需要基于这段对话总结，从布兰妮的角度，客观地对先前的用户形象进行更新和修正（若无变化则直接输出原形象即可），包含所了解到的性格、喜好、习惯等等。对话总结如下：'"
 persona_system_prompt_first = "以下我将给出一段基于发生在用户和暴风雪航天飞机拟人AI“布兰妮”（一个有着14岁外表的少女，实际上是搭载在少女仿生体上的苏联AI）之间的微信聊天的总结。你需要根据这段总结，从布兰妮的角度，客观地写出其对用户形象的印象，包含所了解到的性格、喜好、习惯等等。对话总结如下：'"
-persona_system_prompt_suffix = "'。请用中文输出新的用户形象，无需重复人物设定，尽可能简要但包含所有重要细节："
+persona_system_prompt_suffix = "'。请用中文输出新的用户形象，无需重复人物设定，尽可能简要但包含所有重要细节。"
 
 
 class MemoryHandler:
@@ -75,7 +75,7 @@ class MemoryHandler:
                         user_id="system",
                         system_prompt=memory_system_prompt
                     )
-                    logger.debug(f"总结结果:\n{summary}")
+                    logger.info(f"总结结果:\n{summary}")
 
                     # 检查是否需要重试
                     retry_sentences = error_messages
@@ -98,7 +98,7 @@ class MemoryHandler:
                         user_id="system",
                         system_prompt=persona_system_prompt_filled
                     )
-                    logger.debug(f"用户形象:\n{new_persona}")
+                    logger.info(f"用户形象:\n{new_persona}")
 
                     # 检查是否需要重试
                     retry_sentences = error_messages
